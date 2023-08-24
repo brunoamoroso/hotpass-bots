d3d12bb476d803b481908fa49c9f341d59e55b1a
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-24 15:47:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //configure to read .env file
-// require("dotenv").config();
-import { Telegraf, TelegrafContext } from "telegraf";
+require("dotenv").config();
+const {Telegraf} = require('telegraf');
 
-export const TOKEN = process.env.TOKEN;
 // const BASE_PATH = (process.env.VERCEL_ENV === 'production') ? "https://telegram-bot-teste-psi.vercel.app" : "";
 
 const bot = new Telegraf(process.env.TOKEN);
@@ -15,14 +14,10 @@ const bot = new Telegraf(process.env.TOKEN);
         port: process.env.PORT,
       },
     })
-    .then(() => console.log("👍 Webhook listening on:", port));
+    .then(() => console.log("👍 Webhook listening on:", process.env.PORT));
 
 //answer when Myself send message
 bot.telegram.sendMessage(6588724288, "Alá hein");
 
 bot.start((ctx) => ctx.reply("VAI COMEÇAR A PUTARIA!"));
 bot.hears("Oi", (ctx) => ctx.reply("Salve bonito"));
-
-console.log(process.env);
-
-console.log('Passou por aqui');
