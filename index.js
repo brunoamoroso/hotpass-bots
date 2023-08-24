76873c32a98b2d38c61a163bbc4f6614bb54853a
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-24 12:31:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //configure to read .env file
-require('dotenv').config();
-import { Telegraf, TelegrafContext } from 'telegraf';
+require("dotenv").config();
+import { Telegraf, TelegrafContext } from "telegraf";
 
 export const TOKEN = process.env.TOKEN;
 // const BASE_PATH = (process.env.VERCEL_ENV === 'production') ? "https://telegram-bot-teste-psi.vercel.app" : "";
@@ -8,31 +8,27 @@ export const TOKEN = process.env.TOKEN;
 const bot = new Telegraf(process.env.TOKEN);
 
 // HANDLE OUR CONNECTION WITH TELEGRAM
-export default async (req, res) => {
-    try{
-        const {body, query, } = req;
-        bot
-        .launch(
-            {
-                webhook:{
-                    domain: process.env.WEBHOOK_DOMAIN, port: process.env.PORT
-                }
-            }
-        )
-        .then(() => console.log('👍 Webhook listening on:', port));
-
-    }catch(error){
-        console.log('❌ Deu problema aqui hein');
-        console.log(error.toString());
-    }
-
-    res.status(200).send('✅ Tudo certo!');
+try {
+  const { body, query } = req;
+  bot
+    .launch({
+      webhook: {
+        domain: process.env.WEBHOOK_DOMAIN,
+        port: process.env.PORT,
+      },
+    })
+    .then(() => console.log("👍 Webhook listening on:", port));
+} catch (error) {
+  console.log("❌ Deu problema aqui hein");
+  console.log(error.toString());
 }
+
+res.status(200).send("✅ Tudo certo!");
 
 //answer when Myself send message
 bot.telegram.sendMessage(6588724288, "Alá hein");
 
-bot.start((ctx) => ctx.reply('VAI COMEÇAR A PUTARIA!'));
-bot.hears('Oi', ctx => ctx.reply('Salve bonito'));
+bot.start((ctx) => ctx.reply("VAI COMEÇAR A PUTARIA!"));
+bot.hears("Oi", (ctx) => ctx.reply("Salve bonito"));
 
 console.log(process.env);
