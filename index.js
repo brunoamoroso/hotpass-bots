4a8eebc61a935790ea1089efa7b651cce840cf6d
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-24 20:29:43 -0300

    testing

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 //configure to read .env file
-require("dotenv").config();
+// require("dotenv").config();
 const {Telegraf} = require('telegraf');
 
-// const BASE_PATH = (process.env.VERCEL_ENV === 'production') ? "https://telegram-bot-teste-psi.vercel.app" : "";
+// // const BASE_PATH = (process.env.VERCEL_ENV === 'production') ? "https://telegram-bot-teste-psi.vercel.app" : "";
 
 const bot = new Telegraf(process.env.TOKEN);
 
-// HANDLE OUR CONNECTION WITH TELEGRAM
-  bot
-    .launch({
-      webhook: {
-        domain: process.env.WEBHOOK_DOMAIN,
-        port: process.env.PORT,
-      },
-    })
-    .then(() => console.log("👍 Webhook listening on:", process.env.PORT));
-
-//answer when Myself send message
 bot.telegram.sendMessage(6588724288, "Alá hein");
 
-bot.start((ctx) => ctx.reply("VAI COMEÇAR A PUTARIA!"));
-bot.hears("Oi", (ctx) => ctx.reply("Salve bonito"));
+module.exports = (req, res) => {
+    try{
+        const { query, body } = req;
+
+
+    }catch(err){
+        console.log(err);
+    }
+
+    res.status(200).send('OK');
+}
+// // HANDLE OUR CONNECTION WITH TELEGRAM
+//   bot
+//     .launch({
+//       webhook: {
+//         domain: process.env.WEBHOOK_DOMAIN,
+//       },
+//     })
+//     .then(() => console.log("👍 Webhook listening on:", process.env.PORT));
+
+// //answer when Myself send message
+// bot.telegram.sendMessage(6588724288, "Alá hein");
+
+// bot.start((ctx) => ctx.reply("VAI COMEÇAR A PUTARIA!"));
+// bot.hears("Oi", (ctx) => ctx.reply("Salve bonito"));
