e307d82096128d59b321a97dc990a16f2804455c
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-25 17:40:24 -0300

    test

--- api/index.mjs
+++ api/index.mjs
@@ -5,6 +5,7 @@ import { configDotenv } from 'dotenv';
 
 configDotenv();
 const app = express();
+
 const BASE_PATH =  "https://telegram-bot-teste-psi.vercel.app";
 
 const bot = new Telegraf(process.env.TOKEN);
@@ -14,9 +15,14 @@ bot.telegram.sendMessage(6588724288, "Alá hein");
 bot.hears('Oi', (ctx) => { ctx.reply('Salve')});
 bot.launch();
 
-export default async (req, res) => {
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.status(200).send('OK');
-}
+})
+export default app;
 
 // app.use(express.urlencoded({
 //     extended: true
