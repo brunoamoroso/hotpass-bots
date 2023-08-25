7d7b25dd6060f752d1d53bb1c8f5e0f7ffef9b39
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-25 17:47:34 -0300

    webhook welldone testing removing

--- api/index.mjs
+++ api/index.mjs
@@ -6,7 +6,7 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 const app = express();
 
-const BASE_PATH =  "https://telegram-bot-teste-psi.vercel.app";
+const BASE_PATH =  "https://telegram-bot-teste-psi.vercel.app/api/";
 
 const bot = new Telegraf(process.env.TOKEN);
 const SECRET_HASH = "32e58fbahey833349df3383dc910e180"
@@ -15,13 +15,11 @@ bot.telegram.sendMessage(6588724288, "Alá hein");
 bot.hears('Oi', (ctx) => { ctx.reply('Salve')});
 bot.launch();
 
+//middleware to enable POST method from telegram
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     res.status(200).send('OK');
 })
+
 export default app;
 
 // app.use(express.urlencoded({
