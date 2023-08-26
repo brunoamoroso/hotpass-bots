ff8cb92d5f18160a3808b8a984fffec3d0bc8ae7
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-25 21:15:54 -0300

    Update index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -21,13 +21,13 @@ app.use(express.json());
 //middleware to enable POST method from telegram
 app.use('/', (req, res) => {
   const {body, query} = req;
-  bot.command('start', ctx => ctx.reply('Hello'));
-  bot.telegram.sendMessage(6588724288, "Alá hein");
-  bot.hears("Oi", (ctx) => {
-    ctx.telegram.sendMessage('salve');
-  });
+  // bot.command('start', ctx => ctx.reply('Hello'));
+  // bot.telegram.sendMessage(6588724288, "Alá hein");
+  // bot.hears("Oi", (ctx) => {
+  //   ctx.telegram.sendMessage('salve');
+  // });
 
-  bot.launch();
+  // bot.launch();
 
   res.status(200).send('Entrei');
 })
