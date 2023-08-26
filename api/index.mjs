e8642f786cf95ca134f51d8cf778366a738e0038
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-26 13:33:52 -0300

    Markup Integration with replys and menu
    A basic testing with inlineMarkupMenus and replies

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Markup } from "telegraf";
 import express from "express";
 import { configDotenv } from "dotenv";
 
@@ -7,45 +7,42 @@ const app = express();
 
 const bot = new Telegraf(process.env.TOKEN);
 // const SECRET_HASH = "32e58fbahey833349df3383dc910e180";
+
+//setting webhook to receive updates
 app.use(await bot.createWebhook({domain: process.env.WEBHOOK_DOMAIN, path: '/api/'}))
-// bot.launch({webhook: {domain: process.env.WEBHOOK_DOMAIN, hookPath: '/api/'}});
 
-bot.start(ctx => ctx.reply('Olá'));
-  bot.telegram.sendMessage(6588724288, "Alá hein");
-  bot.hears('Oi', ctx => ctx.reply('Mandou oi'));
+//local route
+// app.use("/", (req, res) => {
+//   bot.launch();
+// });
 
-// app.use('/', (req, res) => {
-//   const {body, query } = req;
+//Initial message and entrypoint
+bot.start((ctx) => {
+  const photo =
+    "https://instagram.fccm2-1.fna.fbcdn.net/v/t51.2885-15/333171007_5984120695037196_245506842773113983_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fccm2-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=Pq5JW1iLYH8AX8hSIkK&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzA1NDk1MjY4NzQxMDY4NTQyMg%3D%3D.2-ccb7-5&oh=00_AfC1Z57_wLEktVizsI5thVi9lcCwM7IqHVOFEelJR6tcFQ&oe=64EE90EB&_nc_sid=ee9879";
+  ctx.replyWithPhoto(photo, {
+    caption: "Oi amor, achou meu bot é? Vem navegar comigo.",
+    parse_mode: "Markdown",
+    ...Markup.inlineKeyboard([
+      Markup.button.callback("🛥 Socar meu Yatch nesse porto", "test"),
+      Markup.button.callback("📹 Meus Packs", "packs"),
+    ])
+      .oneTime()
+      .resize(),
+  });
+});
 
-//   bot.start(ctx => ctx.reply('Olá'));
-//   bot.telegram.sendMessage(6588724288, "Alá hein");
-//   bot.hears('Oi', ctx => ctx.reply('Mandou oi'));
-//   res.send('Olá');
-// })
+bot.action("test", (ctx) => {
+  ctx.reply("Oxi, calma lá!");
+});
 
+bot.action("packs", (ctx) => {
+    ctx.reply("Tá querendo me ver é?");
+  });
+
+// bot.telegram.sendMessage(6588724288, "Alá hein");
+bot.hears("Oi", (ctx) => ctx.reply("Mandou oi"));
+
+app.listen(3000);
 
 export default app;
-// app.post('/', (req, res) => {
-//     bot.launch({
-//         webhook: { domain: process.env.TOKEN}
-//     })
-//     res.status(200).send('ok');
-// })
-
-// app.listen(3000, () => { console.log('listening 3000')});
-
-// app.use(bot.web, (req, res) => {
-//   const {body, query} = req;
-//   bot.launch();
-
-
-
-//   bot.start(ctx => ctx.reply('Hello'));
-//   bot.telegram.sendMessage(6588724288, "Alá hein");
-//   bot.hears("Oi", (ctx) => {
-//     ctx.telegram.sendMessage('salve');
-//   });
-
-
-//   res.send('Olá');
-// })
