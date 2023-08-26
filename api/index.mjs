0535c665ae9f36ad68d89d56310f7618b05c32f9
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-08-25 22:13:31 -0300

    webhook on middleware

--- api/index.mjs
+++ api/index.mjs
@@ -9,7 +9,23 @@ const BASE_PATH = "https://telegram-bot-teste-psi.vercel.app/api/";
 
 const bot = new Telegraf(process.env.TOKEN);
 // const SECRET_HASH = "32e58fbahey833349df3383dc910e180";
-app.use(await bot.createWebhook({domain: process.env.WEBHOOK_DOMAIN}))
+
+app.use(await bot.createWebhook({domain: process.env.WEBHOOK_DOMAIN}), (req, res) => {
+  const {body, query} = req;
+  bot.launch();
+
+
+
+  bot.start(ctx => ctx.reply('Hello'));
+  bot.telegram.sendMessage(6588724288, "Alá hein");
+  bot.hears("Oi", (ctx) => {
+    ctx.telegram.sendMessage('salve');
+  });
+
+
+  res.send('Olá');
+})
+
 
 export default app;
 // app.post('/', (req, res) => {
