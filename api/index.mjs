6c4b90fda0e677f22f572cd1582321440de8ad34
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-09-08 19:01:07 -0300

    createAd flow

--- api/index.mjs
+++ api/index.mjs
@@ -9,6 +9,7 @@ configDotenv();
 const app = express();
 
 const bot = new Telegraf(process.env.TOKEN);
+const ads = new AdsController();
 // const SECRET_HASH = "32e58fbahey833349df3383dc910e180";
 
 //setting webhook to receive updates
@@ -21,8 +22,8 @@ bot.start((ctx) => {
   const photo =
     "https://instagram.fccm2-1.fna.fbcdn.net/v/t51.2885-15/333171007_5984120695037196_245506842773113983_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fccm2-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=Pq5JW1iLYH8AX8hSIkK&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzA1NDk1MjY4NzQxMDY4NTQyMg%3D%3D.2-ccb7-5&oh=00_AfC1Z57_wLEktVizsI5thVi9lcCwM7IqHVOFEelJR6tcFQ&oe=64EE90EB&_nc_sid=ee9879";
   ctx.replyWithPhoto(photo, {
-    caption: "Oi amor, achou meu bot é? Vem navegar comigo.",
-    parse_mode: "Markdown",
+    caption: "Oi amor, achou meu bot é? Vem navegar comigo\\.",
+    parse_mode: "MarkdownV2",
     ...Markup.inlineKeyboard([
       Markup.button.callback("📢 Anúncios", "ads"),
       Markup.button.callback("🛥 Socar meu Yatch nesse porto", "test"),
@@ -37,29 +38,21 @@ bot.start((ctx) => {
 const { enter, leave } = Scenes.Stage;
 
 //create scenes
-const adsScene = new Scenes.BaseScene('AdsScene');
+// const adsScene = new Scenes.BaseScene('AdsScene');
 
 //creates stage
-const stage = new Scenes.Stage([adsScene]);
+const stage = new Scenes.Stage([ads.createAdsWizard]);
 bot.use(session());
 bot.use(stage.middleware());
 
-adsScene.enter((ctx) => {
-  const ads = new AdsController(ctx);
-  ads.sendAdsMenu();
-})
-
-adsScene.action('createAd', (ctx) => {
-  ctx.reply('Certo, comece me enviando uma foto');
-})
-
-adsScene.on('message', (ctx) => {
-  ads.setPreviewContent();
-})
-
 bot.action("ads", (ctx) => {
-  ctx.scene.enter('AdsScene');
+  ads.setCtx(ctx);
+  ads.sendAdsMenu();
 });
+
+bot.action("createAd", (ctx) => {
+  ctx.scene.enter('createAdsScene');
+})
 
 
 // bot.action("packs", (ctx) => {
@@ -67,7 +60,7 @@ bot.action("ads", (ctx) => {
 //   });
 
 // bot.telegram.sendMessage(6588724288, "Alá hein");
-bot.hears("Oi", (ctx) => ctx.reply("Mandou oi"));
+// bot.hears("Oi", (ctx) => ctx.reply("Mandou oi"));
 
 //local route
 app.use("/", (req, res) => {
