88c2f8e6332567aa775a351007c8ae764a718db3
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-10-19 11:14:36 -0300

    deploy 2

--- api/models/linkAgreggatorModel.mjs
+++ api/models/linkAgreggatorModel.mjs
@@ -1,50 +1,50 @@
-import client from "../db/conn.mjs";
+// import client from "../db/conn.mjs";
 
-export const saveLink = async (linkData) => {
-  try {
-    await client.execute(
-      `insert Links{
-                name := <str>$name,
-                url := <str>$url
-            }`,
-      {
-        name: linkData.name,
-        url: linkData.url,
-      }
-    );
+// export const saveLink = async (linkData) => {
+//   try {
+//     await client.execute(
+//       `insert Links{
+//                 name := <str>$name,
+//                 url := <str>$url
+//             }`,
+//       {
+//         name: linkData.name,
+//         url: linkData.url,
+//       }
+//     );
 
-    return { message: "Link Salvo!", status: 200 };
-  } catch (err) {
-    return { message: "Tivemos um problema", status: 500 };
-  }
-};
+//     return { message: "Link Salvo!", status: 200 };
+//   } catch (err) {
+//     return { message: "Tivemos um problema", status: 500 };
+//   }
+// };
 
-export const getLinks = async () => {
-  try {
-    const links = await client.query(`
-                select Links {*};
-            `);
+// export const getLinks = async () => {
+//   try {
+//     const links = await client.query(`
+//                 select Links {*};
+//             `);
 
-    return links;
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     return links;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-export const deleteLink = async (linkId) => {
-  try {
-    await client.execute(
-      `
-                delete Links filter .id = <uuid>$id
-            `,
-      {
-        id: linkId,
-      }
-    );
+// export const deleteLink = async (linkId) => {
+//   try {
+//     await client.execute(
+//       `
+//                 delete Links filter .id = <uuid>$id
+//             `,
+//       {
+//         id: linkId,
+//       }
+//     );
 
-    return { message: "Link Excluído!", status: 200 };
-  } catch (err) {
-    return { message: "Tivemos um problema!", status: 500 };
-    console.log(err);
-  }
-};
+//     return { message: "Link Excluído!", status: 200 };
+//   } catch (err) {
+//     return { message: "Tivemos um problema!", status: 500 };
+//     console.log(err);
+//   }
+// };
--- api/models/subscriptionsModel.mjs
+++ api/models/subscriptionsModel.mjs
@@ -1,66 +1,66 @@
-import client from "../db/conn.mjs";
+// import client from "../db/conn.mjs";
 
-export const save = async (plan) => {
-  try {
-    return await client.execute(
-      `
-            insert Subscriptions{
-                title := <str>$title,
-                price := <str>$price,
-                duration := <int16>$duration,
-                cycle := <str>$cycle,
-                status := <str>"enabled",
-                created_at := <datetime>$createdAt,
-                updated_at := <datetime>$updatedAt,
-            }
-        `,
-      {
-        title: plan.title,
-        price: plan.price,
-        duration: parseInt(plan.duration),
-        cycle: plan.cycle,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      }
-    );
-  } catch (e) {
-    console.log(e);
-  }
-};
+// export const save = async (plan) => {
+//   try {
+//     return await client.execute(
+//       `
+//             insert Subscriptions{
+//                 title := <str>$title,
+//                 price := <str>$price,
+//                 duration := <int16>$duration,
+//                 cycle := <str>$cycle,
+//                 status := <str>"enabled",
+//                 created_at := <datetime>$createdAt,
+//                 updated_at := <datetime>$updatedAt,
+//             }
+//         `,
+//       {
+//         title: plan.title,
+//         price: plan.price,
+//         duration: parseInt(plan.duration),
+//         cycle: plan.cycle,
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       }
+//     );
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
 
-export const getActivePlansAdmin = async () => {
-  return await client.query(`
-      select Subscriptions {
-        title,
-        price,
-        duration,
-        cycle,
-        subscribers := (select count((select User filter User.subscriptions_bought.title = Subscriptions.title AND User.subscriptions_bought@isActive = true)))
-      };
-    `);
-};
+// export const getActivePlansAdmin = async () => {
+//   return await client.query(`
+//       select Subscriptions {
+//         title,
+//         price,
+//         duration,
+//         cycle,
+//         subscribers := (select count((select User filter User.subscriptions_bought.title = Subscriptions.title AND User.subscriptions_bought@isActive = true)))
+//       };
+//     `);
+// };
 
-export const getActivePlansCustomer = async () => {
-  return await client.query(`
-      select Subscriptions {
-        id,
-        title,
-        price,
-        duration,
-        cycle,
-      } filter .status = Status.enabled;
-    `);
-};
+// export const getActivePlansCustomer = async () => {
+//   return await client.query(`
+//       select Subscriptions {
+//         id,
+//         title,
+//         price,
+//         duration,
+//         cycle,
+//       } filter .status = Status.enabled;
+//     `);
+// };
 
-export const getSubscriptionPlanById = async (id) => {
-  return await client.querySingle(`
-      select Subscriptions{
-        title,
-        price,
-        duration,
-        cycle
-      } filter .id = <uuid>$id
-  `, {
-    id: id,
-  });
-}
+// export const getSubscriptionPlanById = async (id) => {
+//   return await client.querySingle(`
+//       select Subscriptions{
+//         title,
+//         price,
+//         duration,
+//         cycle
+//       } filter .id = <uuid>$id
+//   `, {
+//     id: id,
+//   });
+// }
