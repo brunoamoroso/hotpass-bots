411052fa699c9aa10865588191926f8a70b6871a
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-10-19 11:14:12 -0300

    just trying to deploy

--- api/controllers/packsController.mjs
+++ api/controllers/packsController.mjs
@@ -1,290 +1,290 @@
-import { Markup, Scenes } from "telegraf";
-import * as packModel from "../models/packsModel.mjs";
-import mainMenu from "../mainMenu.mjs";
+// import { Markup, Scenes } from "telegraf";
+// import * as packModel from "../models/packsModel.mjs";
+// import mainMenu from "../mainMenu.mjs";
 
-export const sendMenu = (ctx) => {
-  ctx.reply("Entendido\\. O que você quer fazer nos Packs?", {
-    parse_mode: "MarkdownV2",
-    ...Markup.inlineKeyboard([
-      [Markup.button.callback("Criar Pack", "createPack")],
-      [Markup.button.callback("Ver Packs Ativos", "viewPacks")],
-    ])
-      .oneTime()
-      .resize(),
-  });
-};
+// export const sendMenu = (ctx) => {
+//   ctx.reply("Entendido\\. O que você quer fazer nos Packs?", {
+//     parse_mode: "MarkdownV2",
+//     ...Markup.inlineKeyboard([
+//       [Markup.button.callback("Criar Pack", "createPack")],
+//       [Markup.button.callback("Ver Packs Ativos", "viewPacks")],
+//     ])
+//       .oneTime()
+//       .resize(),
+//   });
+// };
 
-export const createPack = new Scenes.BaseScene("createPackScene");
+// export const createPack = new Scenes.BaseScene("createPackScene");
 
-createPack.enter((ctx) => {
-  ctx.reply(
-    "Comece me enviando a prévia do conteúdo. Pode ser uma foto ou um vídeo"
-  );
-  ctx.scene.session.step = 0;
-  ctx.scene.session.packData = { user_id: ctx.chat.id };
-});
+// createPack.enter((ctx) => {
+//   ctx.reply(
+//     "Comece me enviando a prévia do conteúdo. Pode ser uma foto ou um vídeo"
+//   );
+//   ctx.scene.session.step = 0;
+//   ctx.scene.session.packData = { user_id: ctx.chat.id };
+// });
 
-createPack.on(
-  "message",
-  (ctx, next) => {
-    //receives preview content
-    if (ctx.scene.session.step === 0) {
-      if (ctx.message.photo) {
-        ctx.scene.session.packData.mediaPreview = ctx.message.photo[0].file_id;
-        ctx.scene.session.packData.mediaPreviewType = "photo";
-        ctx.scene.session.step = 1;
-        next();
-      } else {
-        ctx.reply(
-          "Desculpa, mas para o preview só aceitamos foto porque o telegram só aceita foto na hora de enviar a cobrança confirmado o produto que está sendo comprado"
-        );
-      }
-    } else {
-      next();
-    }
-  },
-  (ctx, next) => {
-    if (ctx.scene.session.step === 1) {
-      ctx.reply("Envie um título para o seu pack");
-      ctx.scene.session.step = 2;
-    } else {
-      next();
-    }
-  },
-  (ctx, next) => {
-    //receives title
-    if (ctx.scene.session.step === 2) {
-      ctx.scene.session.packData.title = ctx.message.text;
-      ctx.reply("Envie uma descrição para o pack");
-      ctx.scene.session.step = 3;
-    } else {
-      next();
-    }
-  },
-  (ctx, next) => {
-    //receives description
-    if (ctx.scene.session.step === 3) {
-      ctx.scene.session.packData.description = ctx.message.text;
-      ctx.reply("Quanto vai custar o pack?");
-      ctx.scene.session.step = 4;
-    } else {
-      next();
-    }
-  },
-  (ctx, next) => {
-    if (ctx.scene.session.step === 4) {
-      ctx.scene.session.packData.price = ctx.message.text;
-      ctx.reply(
-        "Agora me envie o conteúdo do pack. \nFotos e vídeos que serão enviados quando o cliente comprar o pack. \n\nQuando você tiver enviado todos os itens do pack e eles estiverem com os dois ✓✓. Então clique no botão abaixo ⤵️",
-        {
-          ...Markup.inlineKeyboard([
-            Markup.button.callback(
-              "✅ Enviei todo o conteúdo",
-              "contentPackDone"
-            ),
-          ]),
-        }
-      );
-      ctx.scene.session.step = 5;
-    } else {
-      next();
-    }
-  },
-  (ctx, next) => {
-    if (ctx.scene.session.step === 5) {
-      if (!ctx.scene.session.packData.content) {
-        if (ctx.message.photo) {
-          ctx.scene.session.packData.content = [
-            { type: "photo", media: ctx.message.photo[0].file_id },
-          ];
-        }
+// createPack.on(
+//   "message",
+//   (ctx, next) => {
+//     //receives preview content
+//     if (ctx.scene.session.step === 0) {
+//       if (ctx.message.photo) {
+//         ctx.scene.session.packData.mediaPreview = ctx.message.photo[0].file_id;
+//         ctx.scene.session.packData.mediaPreviewType = "photo";
+//         ctx.scene.session.step = 1;
+//         next();
+//       } else {
+//         ctx.reply(
+//           "Desculpa, mas para o preview só aceitamos foto porque o telegram só aceita foto na hora de enviar a cobrança confirmado o produto que está sendo comprado"
+//         );
+//       }
+//     } else {
+//       next();
+//     }
+//   },
+//   (ctx, next) => {
+//     if (ctx.scene.session.step === 1) {
+//       ctx.reply("Envie um título para o seu pack");
+//       ctx.scene.session.step = 2;
+//     } else {
+//       next();
+//     }
+//   },
+//   (ctx, next) => {
+//     //receives title
+//     if (ctx.scene.session.step === 2) {
+//       ctx.scene.session.packData.title = ctx.message.text;
+//       ctx.reply("Envie uma descrição para o pack");
+//       ctx.scene.session.step = 3;
+//     } else {
+//       next();
+//     }
+//   },
+//   (ctx, next) => {
+//     //receives description
+//     if (ctx.scene.session.step === 3) {
+//       ctx.scene.session.packData.description = ctx.message.text;
+//       ctx.reply("Quanto vai custar o pack?");
+//       ctx.scene.session.step = 4;
+//     } else {
+//       next();
+//     }
+//   },
+//   (ctx, next) => {
+//     if (ctx.scene.session.step === 4) {
+//       ctx.scene.session.packData.price = ctx.message.text;
+//       ctx.reply(
+//         "Agora me envie o conteúdo do pack. \nFotos e vídeos que serão enviados quando o cliente comprar o pack. \n\nQuando você tiver enviado todos os itens do pack e eles estiverem com os dois ✓✓. Então clique no botão abaixo ⤵️",
+//         {
+//           ...Markup.inlineKeyboard([
+//             Markup.button.callback(
+//               "✅ Enviei todo o conteúdo",
+//               "contentPackDone"
+//             ),
+//           ]),
+//         }
+//       );
+//       ctx.scene.session.step = 5;
+//     } else {
+//       next();
+//     }
+//   },
+//   (ctx, next) => {
+//     if (ctx.scene.session.step === 5) {
+//       if (!ctx.scene.session.packData.content) {
+//         if (ctx.message.photo) {
+//           ctx.scene.session.packData.content = [
+//             { type: "photo", media: ctx.message.photo[0].file_id },
+//           ];
+//         }
 
-        if (ctx.message.video) {
-          ctx.scene.session.packData.content = [
-            { type: "video", media: ctx.message.video.file_id },
-          ];
-        }
-      } else {
-        if (ctx.message.photo) {
-          ctx.scene.session.packData.content.push({
-            type: "photo",
-            media: ctx.message.photo[0].file_id,
-          });
-        }
+//         if (ctx.message.video) {
+//           ctx.scene.session.packData.content = [
+//             { type: "video", media: ctx.message.video.file_id },
+//           ];
+//         }
+//       } else {
+//         if (ctx.message.photo) {
+//           ctx.scene.session.packData.content.push({
+//             type: "photo",
+//             media: ctx.message.photo[0].file_id,
+//           });
+//         }
 
-        if (ctx.message.video) {
-          ctx.scene.session.packData.content.push({
-            type: "video",
-            media: ctx.message.video.file_id,
-          });
-        }
-      }
-    } else {
-      next();
-    }
-  }
-);
+//         if (ctx.message.video) {
+//           ctx.scene.session.packData.content.push({
+//             type: "video",
+//             media: ctx.message.video.file_id,
+//           });
+//         }
+//       }
+//     } else {
+//       next();
+//     }
+//   }
+// );
 
-createPack.action("contentPackDone", async (ctx) => {
-  await ctx.reply("Certo, vamos para a revisão");
-  ctx.scene.session.step = 6;
+// createPack.action("contentPackDone", async (ctx) => {
+//   await ctx.reply("Certo, vamos para a revisão");
+//   ctx.scene.session.step = 6;
 
-  if (ctx.scene.session.packData.mediaPreviewType === "photo") {
-    await ctx.replyWithPhoto(ctx.scene.session.packData.mediaPreview, {
-      parse_mode: "MarkdownV2",
-      caption:
-        ctx.scene.session.packData.title.replace(".", "\\.") +
-        "\n" +
-        ctx.scene.session.packData.description.replace(".", "\\."),
-    });
-  }
+//   if (ctx.scene.session.packData.mediaPreviewType === "photo") {
+//     await ctx.replyWithPhoto(ctx.scene.session.packData.mediaPreview, {
+//       parse_mode: "MarkdownV2",
+//       caption:
+//         ctx.scene.session.packData.title.replace(".", "\\.") +
+//         "\n" +
+//         ctx.scene.session.packData.description.replace(".", "\\."),
+//     });
+//   }
 
-  if (ctx.scene.session.packData.mediaPreviewType === "video") {
-    await ctx.replyWithVideo(ctx.scene.session.packData.mediaPreview, {
-      parse_mode: "MarkdownV2",
-      caption: ctx.scene.session.packData.description,
-    });
-  }
+//   if (ctx.scene.session.packData.mediaPreviewType === "video") {
+//     await ctx.replyWithVideo(ctx.scene.session.packData.mediaPreview, {
+//       parse_mode: "MarkdownV2",
+//       caption: ctx.scene.session.packData.description,
+//     });
+//   }
 
-  await ctx.reply(
-    "*_O preço que será cobrado:_*\nR$" +
-      ctx.scene.session.packData.price.replace(".", "\\."),
-    {
-      parse_mode: "MarkdownV2",
-    }
-  );
+//   await ctx.reply(
+//     "*_O preço que será cobrado:_*\nR$" +
+//       ctx.scene.session.packData.price.replace(".", "\\."),
+//     {
+//       parse_mode: "MarkdownV2",
+//     }
+//   );
 
-  await ctx.reply("*_O conteúdo que será enviado na compra_*", {
-    parse_mode: "MarkdownV2",
-  });
-  await ctx.replyWithMediaGroup(ctx.scene.session.packData.content);
+//   await ctx.reply("*_O conteúdo que será enviado na compra_*", {
+//     parse_mode: "MarkdownV2",
+//   });
+//   await ctx.replyWithMediaGroup(ctx.scene.session.packData.content);
 
-  await ctx.reply("O que deseja fazer?", {
-    ...Markup.inlineKeyboard([
-      [Markup.button.callback("✅ Salvar", "save")],
-      [Markup.button.callback("❌ Descartar", "cancel")],
-    ]),
-  });
-});
+//   await ctx.reply("O que deseja fazer?", {
+//     ...Markup.inlineKeyboard([
+//       [Markup.button.callback("✅ Salvar", "save")],
+//       [Markup.button.callback("❌ Descartar", "cancel")],
+//     ]),
+//   });
+// });
 
-createPack.action("save", (ctx) => {
-  packModel.savePack(ctx.scene.session.packData).then((resp) => {
-    ctx.reply(resp.message);
-  });
-});
+// createPack.action("save", (ctx) => {
+//   packModel.savePack(ctx.scene.session.packData).then((resp) => {
+//     ctx.reply(resp.message);
+//   });
+// });
 
-createPack.action("cancel", async (ctx) => {
-  await ctx.reply("Saindo da Criação de Packs \n________________________");
-  ctx.scene.leave();
-});
+// createPack.action("cancel", async (ctx) => {
+//   await ctx.reply("Saindo da Criação de Packs \n________________________");
+//   ctx.scene.leave();
+// });
 
-createPack.leave((ctx) => {
-  mainMenu(ctx);
-});
+// createPack.leave((ctx) => {
+//   mainMenu(ctx);
+// });
 
-export const viewPacks = new Scenes.BaseScene("viewPacksScene");
+// export const viewPacks = new Scenes.BaseScene("viewPacksScene");
 
-viewPacks.enter((ctx) => {
-  packModel.getPacks().then((packs) => {
-    packs.forEach((pack) => {
-      if (pack.media_preview_type === "photo") {
-        ctx.replyWithPhoto(pack.media_preview, {
-          parse_mode: "MarkdownV2",
-          caption:
-            pack.description +
-            `\n\n\n\*_Preço cobrado pelo pack: _*\n${pack.price.replace(
-              ".",
-              "\\."
-            )}`,
-          ...Markup.inlineKeyboard([
-            [Markup.button.callback("👀 Ver conteúdos do Pack", "viewContent")],
-            [Markup.button.callback("❌ Desativar Pack", "disablePack")],
-          ]),
-        });
-      }
+// viewPacks.enter((ctx) => {
+//   packModel.getPacks().then((packs) => {
+//     packs.forEach((pack) => {
+//       if (pack.media_preview_type === "photo") {
+//         ctx.replyWithPhoto(pack.media_preview, {
+//           parse_mode: "MarkdownV2",
+//           caption:
+//             pack.description +
+//             `\n\n\n\*_Preço cobrado pelo pack: _*\n${pack.price.replace(
+//               ".",
+//               "\\."
+//             )}`,
+//           ...Markup.inlineKeyboard([
+//             [Markup.button.callback("👀 Ver conteúdos do Pack", "viewContent")],
+//             [Markup.button.callback("❌ Desativar Pack", "disablePack")],
+//           ]),
+//         });
+//       }
 
-      if (pack.media_preview_type === "video") {
-        ctx.replyWithVideo(pack.media_preview, {
-          parse_mode: "MarkdownV2",
-          caption:
-            pack.description +
-            `\n\n\n\*_Preço cobrado pelo pack: _*\n${pack.price.replace(
-              ".",
-              "\\."
-            )}`,
-          ...Markup.inlineKeyboard([
-            [Markup.button.callback("👀 Ver conteúdos do Pack", "viewContent")],
-            [Markup.button.callback("❌ Desativar Pack", "disablePack")],
-          ]),
-        });
-      }
-    });
-  });
-});
+//       if (pack.media_preview_type === "video") {
+//         ctx.replyWithVideo(pack.media_preview, {
+//           parse_mode: "MarkdownV2",
+//           caption:
+//             pack.description +
+//             `\n\n\n\*_Preço cobrado pelo pack: _*\n${pack.price.replace(
+//               ".",
+//               "\\."
+//             )}`,
+//           ...Markup.inlineKeyboard([
+//             [Markup.button.callback("👀 Ver conteúdos do Pack", "viewContent")],
+//             [Markup.button.callback("❌ Desativar Pack", "disablePack")],
+//           ]),
+//         });
+//       }
+//     });
+//   });
+// });
 
-export const buyPacks = new Scenes.BaseScene("buyPacksScene");
+// export const buyPacks = new Scenes.BaseScene("buyPacksScene");
 
-buyPacks.enter((ctx) => {
-  ctx.session.chat = ctx.chat.id;
-  packModel.getPacks().then((packs) => {
-    packs.forEach((pack) => {
-      if (pack.media_preview_type === "photo") {
-        ctx.replyWithPhoto(pack.media_preview, {
-          parse_mode: "Markdownv2",
-          caption:
-            "*" +
-            pack.title.replace(".", "\\.") +
-            "*" +
-            "\n\n" +
-            pack.description.replace(".", "\\."),
-          ...Markup.inlineKeyboard([
-            Markup.button.callback("Comprar - R$" + pack.price, pack.id),
-          ]),
-        });
-      } else if (pack.media_preview_type === "video") {
-      }
-    });
-  });
-});
+// buyPacks.enter((ctx) => {
+//   ctx.session.chat = ctx.chat.id;
+//   packModel.getPacks().then((packs) => {
+//     packs.forEach((pack) => {
+//       if (pack.media_preview_type === "photo") {
+//         ctx.replyWithPhoto(pack.media_preview, {
+//           parse_mode: "Markdownv2",
+//           caption:
+//             "*" +
+//             pack.title.replace(".", "\\.") +
+//             "*" +
+//             "\n\n" +
+//             pack.description.replace(".", "\\."),
+//           ...Markup.inlineKeyboard([
+//             Markup.button.callback("Comprar - R$" + pack.price, pack.id),
+//           ]),
+//         });
+//       } else if (pack.media_preview_type === "video") {
+//       }
+//     });
+//   });
+// });
 
-buyPacks.on("callback_query", async (ctx) => {
-  const pack = await packModel.getPackById(ctx.callbackQuery.data);
-  ctx.sendInvoice({
-    photo_url: await ctx.replyWithPhoto(pack.media_preview),
-    chat_id: ctx.chat.id,
-    title: "Pack",
-    description: `Esse pack contém ${pack.contentQty} itens para você`,
-    payload: { userId: ctx.chat.id, packId: ctx.callbackQuery.data },
-    provider_token: process.env.STRIPE_KEY,
-    currency: "BRL",
-    prices: [{ label: "Preço", amount: pack.price.replace(".", "") }],
-  });
+// buyPacks.on("callback_query", async (ctx) => {
+//   const pack = await packModel.getPackById(ctx.callbackQuery.data);
+//   ctx.sendInvoice({
+//     photo_url: await ctx.replyWithPhoto(pack.media_preview),
+//     chat_id: ctx.chat.id,
+//     title: "Pack",
+//     description: `Esse pack contém ${pack.contentQty} itens para você`,
+//     payload: { userId: ctx.chat.id, packId: ctx.callbackQuery.data },
+//     provider_token: process.env.STRIPE_KEY,
+//     currency: "BRL",
+//     prices: [{ label: "Preço", amount: pack.price.replace(".", "") }],
+//   });
 
-  ctx.scene.session.pack_bought = pack.id;
-});
+//   ctx.scene.session.pack_bought = pack.id;
+// });
 
-buyPacks.on("pre_checkout_query", (ctx) => {
-  ctx.answerPreCheckoutQuery(true);
-});
+// buyPacks.on("pre_checkout_query", (ctx) => {
+//   ctx.answerPreCheckoutQuery(true);
+// });
 
-buyPacks.on("message", (ctx) => {
-  if (ctx.message.successful_payment) {
-    ctx.reply("Toma meu pack 😏");
-    const payload = JSON.parse(ctx.message.successful_payment.invoice_payload);
-    packModel.getPackContentById(payload.packId).then((pack) => {
-      let adjustedPackContent = [];
-      pack[0].content.forEach((packContent) => {
-        adjustedPackContent.push({
-          media: packContent.media_id,
-          type: packContent.media_type,
-        });
-      });
+// buyPacks.on("message", (ctx) => {
+//   if (ctx.message.successful_payment) {
+//     ctx.reply("Toma meu pack 😏");
+//     const payload = JSON.parse(ctx.message.successful_payment.invoice_payload);
+//     packModel.getPackContentById(payload.packId).then((pack) => {
+//       let adjustedPackContent = [];
+//       pack[0].content.forEach((packContent) => {
+//         adjustedPackContent.push({
+//           media: packContent.media_id,
+//           type: packContent.media_type,
+//         });
+//       });
 
-      ctx.sendMediaGroup(adjustedPackContent, {
-        protect_content: true,
-      });
+//       ctx.sendMediaGroup(adjustedPackContent, {
+//         protect_content: true,
+//       });
 
-      packModel.insertPackBought(payload.userId, payload.packId);
-    });
-  }
-});
+//       packModel.insertPackBought(payload.userId, payload.packId);
+//     });
+//   }
+// });
--- api/controllers/subscriptionsController.mjs
+++ api/controllers/subscriptionsController.mjs
@@ -1,125 +1,125 @@
-import { Markup, Scenes } from "telegraf";
-import * as subscriptionsModel from "../models/subscriptionsModel.mjs";
-import cycleFormat from "../utils/cycleFormat.mjs";
+// import { Markup, Scenes } from "telegraf";
+// import * as subscriptionsModel from "../models/subscriptionsModel.mjs";
+// import cycleFormat from "../utils/cycleFormat.mjs";
 
-export const sendMenu = (ctx) => {
-  ctx.reply("O que você quer fazer nas assinaturas?", {
-    ...Markup.inlineKeyboard([
-      [Markup.button.callback("Criar um Plano", "createSubscriptionPlan")],
-      [Markup.button.callback("Ver Planos Ativos", "viewActivePlans")],
-    ]).oneTime(),
-  });
-};
+// export const sendMenu = (ctx) => {
+//   ctx.reply("O que você quer fazer nas assinaturas?", {
+//     ...Markup.inlineKeyboard([
+//       [Markup.button.callback("Criar um Plano", "createSubscriptionPlan")],
+//       [Markup.button.callback("Ver Planos Ativos", "viewActivePlans")],
+//     ]).oneTime(),
+//   });
+// };
 
-export const createSubscription = new Scenes.WizardScene(
-  "createSubscriptionPlanScene",
-  (ctx) => {
-    ctx.scene.session.planData = {};
-    ctx.reply("Certo, comece me enviando qual será o nome do seu plano");
-    ctx.wizard.next();
-  },
-  (ctx) => {
-    ctx.scene.session.planData.title = ctx.message.text;
-    ctx.reply("Agora me fale quanto vai custar o plano de assinatura");
-    ctx.wizard.next();
-  },
-  (ctx) => {
-    ctx.scene.session.planData.price = ctx.message.text;
-    ctx.reply(
-      "O plano será cobrado dos assinates a cada X Semanas ou X Meses?",
-      {
-        ...Markup.inlineKeyboard([
-          [Markup.button.callback("Semanas", "weekly")],
-          [Markup.button.callback("Meses", "monthly")],
-        ]),
-      }
-    );
-    ctx.wizard.next();
-  },
-  (ctx) => {
-    ctx.scene.session.planData.cycle = ctx.callbackQuery.data;
-    const chosenOpt = ctx.callbackQuery.data === "weekly" ? "Semanas" : "Meses";
-    ctx.reply(
-      `\*Você escolheu:  ${chosenOpt}*\n\nAgora digite após quantos\\(as\\) ${chosenOpt} será cobrado o pagamento para continuar acessando o plano\\. \n\nEx: 2 para 2 ${chosenOpt}`,
-      {
-        parse_mode: "MarkdownV2",
-      }
-    );
+// export const createSubscription = new Scenes.WizardScene(
+//   "createSubscriptionPlanScene",
+//   (ctx) => {
+//     ctx.scene.session.planData = {};
+//     ctx.reply("Certo, comece me enviando qual será o nome do seu plano");
+//     ctx.wizard.next();
+//   },
+//   (ctx) => {
+//     ctx.scene.session.planData.title = ctx.message.text;
+//     ctx.reply("Agora me fale quanto vai custar o plano de assinatura");
+//     ctx.wizard.next();
+//   },
+//   (ctx) => {
+//     ctx.scene.session.planData.price = ctx.message.text;
+//     ctx.reply(
+//       "O plano será cobrado dos assinates a cada X Semanas ou X Meses?",
+//       {
+//         ...Markup.inlineKeyboard([
+//           [Markup.button.callback("Semanas", "weekly")],
+//           [Markup.button.callback("Meses", "monthly")],
+//         ]),
+//       }
+//     );
+//     ctx.wizard.next();
+//   },
+//   (ctx) => {
+//     ctx.scene.session.planData.cycle = ctx.callbackQuery.data;
+//     const chosenOpt = ctx.callbackQuery.data === "weekly" ? "Semanas" : "Meses";
+//     ctx.reply(
+//       `\*Você escolheu:  ${chosenOpt}*\n\nAgora digite após quantos\\(as\\) ${chosenOpt} será cobrado o pagamento para continuar acessando o plano\\. \n\nEx: 2 para 2 ${chosenOpt}`,
+//       {
+//         parse_mode: "MarkdownV2",
+//       }
+//     );
 
-    ctx.wizard.next();
-  },
-  async (ctx) => {
-    ctx.scene.session.planData.duration = ctx.message.text;
-    await subscriptionsModel.save(ctx.scene.session.planData);
-    ctx.reply("Plano salvo!");
-    ctx.scene.leave();
-  }
-);
+//     ctx.wizard.next();
+//   },
+//   async (ctx) => {
+//     ctx.scene.session.planData.duration = ctx.message.text;
+//     await subscriptionsModel.save(ctx.scene.session.planData);
+//     ctx.reply("Plano salvo!");
+//     ctx.scene.leave();
+//   }
+// );
 
-export const viewActiveSubscriptions = new Scenes.BaseScene(
-  "viewActiveSubscriptionsScene"
-);
+// export const viewActiveSubscriptions = new Scenes.BaseScene(
+//   "viewActiveSubscriptionsScene"
+// );
 
-viewActiveSubscriptions.enter(async (ctx) => {
-  const plans = await subscriptionsModel.getActivePlansAdmin();
-  plans.forEach((plan) => {
-    plan.cycle = plan.cycle === "weekly" ? "Semanas" : "Meses";
-    ctx.reply(
-      "*_Nome do Plano:_*\n" +
-        plan.title +
-        "\n\n*_Preço do Plano:_*\n" +
-        plan.price.replace(".", "\\.") +
-        "\n\n*_Ciclo de cobrança do Plano:_*\n" +
-        plan.duration +
-        " " +
-        cycleFormat(plan.duration, plan.cycle) +
-        "\n\n*_Assinantes Ativos:_*\n" +
-        plan.subscribers,
-      {
-        parse_mode: "MarkdownV2",
-      }
-    );
-  });
-});
+// viewActiveSubscriptions.enter(async (ctx) => {
+//   const plans = await subscriptionsModel.getActivePlansAdmin();
+//   plans.forEach((plan) => {
+//     plan.cycle = plan.cycle === "weekly" ? "Semanas" : "Meses";
+//     ctx.reply(
+//       "*_Nome do Plano:_*\n" +
+//         plan.title +
+//         "\n\n*_Preço do Plano:_*\n" +
+//         plan.price.replace(".", "\\.") +
+//         "\n\n*_Ciclo de cobrança do Plano:_*\n" +
+//         plan.duration +
+//         " " +
+//         cycleFormat(plan.duration, plan.cycle) +
+//         "\n\n*_Assinantes Ativos:_*\n" +
+//         plan.subscribers,
+//       {
+//         parse_mode: "MarkdownV2",
+//       }
+//     );
+//   });
+// });
 
-export const buySubscription = new Scenes.BaseScene("buySubscriptionScene");
+// export const buySubscription = new Scenes.BaseScene("buySubscriptionScene");
 
-buySubscription.enter((ctx) => {
-  subscriptionsModel.getActivePlansCustomer().then((plans) => {
-    let keyboardBtns = [];
-    plans.forEach((plan) => {
-      const btnText =
-        plan.title +
-        " - " +
-        plan.duration +
-        " " +
-        cycleFormat(plan.duration, plan.cycle);
-      keyboardBtns.push([Markup.button.callback(btnText, plan.id)]);
-    });
+// buySubscription.enter((ctx) => {
+//   subscriptionsModel.getActivePlansCustomer().then((plans) => {
+//     let keyboardBtns = [];
+//     plans.forEach((plan) => {
+//       const btnText =
+//         plan.title +
+//         " - " +
+//         plan.duration +
+//         " " +
+//         cycleFormat(plan.duration, plan.cycle);
+//       keyboardBtns.push([Markup.button.callback(btnText, plan.id)]);
+//     });
 
-    ctx.reply(
-      "Tá quase tendo o privilégio de poucos amor. Escolhe por quanto tempo você quer acesso a mim 😏",
-      {
-        ...Markup.inlineKeyboard(keyboardBtns),
-      }
-    );
-  });
-});
+//     ctx.reply(
+//       "Tá quase tendo o privilégio de poucos amor. Escolhe por quanto tempo você quer acesso a mim 😏",
+//       {
+//         ...Markup.inlineKeyboard(keyboardBtns),
+//       }
+//     );
+//   });
+// });
 
-buySubscription.on("callback_query", async (ctx) => {
-  subscriptionsModel
-    .getSubscriptionPlanById(ctx.callbackQuery.data)
-    .then((plan) => {
-      ctx.sendInvoice(
-        {
-          chat_id: ctx.chat.id,
-          title: plan.title,
-          description: `Vai garantir ${plan.duration} ${cycleFormat(plan.duration, plan.cycle)} a mim amor`,
-          payload: {userId: ctx.chat.id, planId: ctx.callbackQuery.data},
-          provider_token: process.env.STRIPE_KEY,
-          currency: "BRL",
-          prices: [{ label: "Preço", amount: plan.price.replace(".", "") }],
-        }
-      );
-    });
-});
+// buySubscription.on("callback_query", async (ctx) => {
+//   subscriptionsModel
+//     .getSubscriptionPlanById(ctx.callbackQuery.data)
+//     .then((plan) => {
+//       ctx.sendInvoice(
+//         {
+//           chat_id: ctx.chat.id,
+//           title: plan.title,
+//           description: `Vai garantir ${plan.duration} ${cycleFormat(plan.duration, plan.cycle)} a mim amor`,
+//           payload: {userId: ctx.chat.id, planId: ctx.callbackQuery.data},
+//           provider_token: process.env.STRIPE_KEY,
+//           currency: "BRL",
+//           prices: [{ label: "Preço", amount: plan.price.replace(".", "") }],
+//         }
+//       );
+//     });
+// });
