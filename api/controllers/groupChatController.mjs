db6c1d779b4bc949dba551fce710d5b1df3737d4
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-01-26 15:09:17 -0300

    Added setPreviewChat and preview and vip are now upserts
    Added the setPreviewChat as the feature was supposed to be and noticed that if another chat was set it would create another document leading to problems so I changed the code to be an upsert instead of a save

--- api/controllers/groupChatController.mjs
+++ api/controllers/groupChatController.mjs
@@ -1,13 +1,27 @@
 import { getModelByTenant } from "../utils/tenantUtils.mjs";
 import botConfigSchema from "../schemas/BotConfig.mjs";
 
-export const setVipGroup = async (ctx) => {
-    try{  
+export const setVipChat = async (ctx) => {
+    try {
         const BotConfig = getModelByTenant(ctx.session.db, "BotConfig", botConfigSchema);
-        const VipGroup = new BotConfig({channel_id: ctx.chat.id});
+        await BotConfig.findOneAndUpdate(undefined, {vip_chat_id: ctx.chat.id}, {
+            new: true,
+            upsert: true,
+        });
+        await ctx.telegram.sendMessage(6588724288, `✅ Grupo VIP foi definido para o ${ctx.chat.type} com o nome de ${ctx.chat.title}`);
+    }catch(err){
+        console.log(err);
+    }
+}
 
-        await VipGroup.save();
-        await ctx.telegram.sendMessage(6588724288, '✅ Grupo Vip foi definido');
+export const setPreviewChat = async (ctx) => {
+    try{
+        const BotConfig = getModelByTenant(ctx.session.db, "BotConfig", botConfigSchema);
+        await BotConfig.findOneAndUpdate(undefined, {preview_chat_id: ctx.chat.id}, {
+            new: true,
+            upsert: true,
+        });
+        await ctx.telegram.sendMessage(6588724288, `✅ Grupo de Prévias foi definido para o ${ctx.chat.type} com o nome de ${ctx.chat.title} `);
     }catch(err){
         console.log(err);
     }
