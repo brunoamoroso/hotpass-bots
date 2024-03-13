e6475e301c64f31c958d9cd809bf642e3d21aa4a
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-03-13 12:28:48 -0300

    adding empty packs msg

--- api/controllers/groupChatController.mjs
+++ api/controllers/groupChatController.mjs
@@ -17,7 +17,7 @@ export const setVipChat = async (ctx) => {
 export const setPreviewChat = async (ctx) => {
     try{
         const BotConfig = getModelByTenant(ctx.session.db, "BotConfig", botConfigSchema);
-        await BotConfig.findOneAndUpdate(undefined, {preview_chat_id: ctx.chat.id}, {
+        const preview = await BotConfig.findOneAndUpdate(undefined, {preview_chat_id: ctx.chat.id}, {
             new: true,
             upsert: true,
         });
