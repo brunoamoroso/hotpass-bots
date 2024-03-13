2064e6929a53bc7a6a245ece62e8d97f4ef82ac1
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-03-13 20:14:54 -0300

    cleaning code in mainMenu

--- api/mainMenu.mjs
+++ api/mainMenu.mjs
@@ -21,48 +21,37 @@ export default async function mainMenu(ctx, userRole){
   }else{
     const BotConfigsModel = getModelByTenant(ctx.session.db, "BotConfig", botConfigSchema);
     const botConfigs = await BotConfigsModel.findOne().lean();
+    const customerKeyboard = [
+      [Markup.button.callback("❤️‍🔥 Assinaturas", "subscriptionsCustomer")],
+      [Markup.button.callback("📹 Packs", "packsCustomer")],
+      [Markup.button.callback("🔗 Links", "linksCustomer")],
+    ];
 
     if(botConfigs.custom_start){
       if(botConfigs.custom_start.media.type === "photo"){
         return ctx.replyWithPhoto(botConfigs.custom_start.media.file, {
           caption: botConfigs.custom_start.caption,
-          ...Markup.inlineKeyboard([
-            [Markup.button.callback("❤️‍🔥 Assinaturas", "subscriptionsCustomer")],
-            [Markup.button.callback("📹 Packs", "packsCustomer")],
-            [Markup.button.callback("🔗 Links", "linksCustomer")],
-          ])
+          ...Markup.inlineKeyboard(customerKeyboard);
         })
       }
 
       if(botConfigs.custom_start.media.type === "video"){
         return ctx.replyWithVideo(botConfigs.custom_start.media.file, {
           caption: botConfigs.customStart.caption,
-          ...Markup.inlineKeyboard([
-            [Markup.button.callback("❤️‍🔥 Assinaturas", "subscriptionsCustomer")],
-            [Markup.button.callback("📹 Packs", "packsCustomer")],
-            [Markup.button.callback("🔗 Links", "linksCustomer")],
-          ])
+          ...Markup.inlineKeyboard(customerKeyboard)
         })
       }
 
       if(botConfigs.custom_start.text){
         return ctx.reply(botConfigs.custom_start.text, {
-          ...Markup.inlineKeyboard([
-            [Markup.button.callback("❤️‍🔥 Assinaturas", "subscriptionsCustomer")],
-            [Markup.button.callback("📹 Packs", "packsCustomer")],
-            [Markup.button.callback("🔗 Links", "linksCustomer")],
-          ])
+          ...Markup.inlineKeyboard(customerKeyboard)
         });
       }
 
     }
 
     return ctx.reply(`Bem-vindo ao meu menu ${ctx.from.first_name}, o que você quer hoje? 😈`, {
-      ...Markup.inlineKeyboard([
-        [Markup.button.callback("❤️‍🔥 Assinaturas", "subscriptionsCustomer")],
-        [Markup.button.callback("📹 Packs", "packsCustomer")],
-        [Markup.button.callback("🔗 Links", "linksCustomer")],
-      ])
+      ...Markup.inlineKeyboard(customerKeyboard)
     });
   }
 };
