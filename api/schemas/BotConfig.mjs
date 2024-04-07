340b704457a9c6b1f172b7a947920e79ac5d50de
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-04-07 18:47:24 -0300

    testing setting defaults for the botConfigs

--- api/schemas/BotConfig.mjs
+++ api/schemas/BotConfig.mjs
@@ -4,18 +4,34 @@ const botConfigSchema = new Schema(
   {
     vip_chat_id: {
       type: String,
+      default: '',
     },
     preview_chat_id: {
       type: String,
+      default: '',
     },
     split_rules: {
       type: Array,
+      default: [
+        {
+          amount: 15,
+          type: 'percentage',
+          recipient_id: 're_clondjs7y23ny019t4kgpw2po',
+          options: {
+            charge_processing_fee: true,
+            charge_remainder_fee: true,
+            liable: false
+          }
+        }
+      ],
     },
     owner_chat_id: {
       type: String,
+      default: '',
     },
     custom_start:{
       type: Object,
+      default: {},
     }
   },
   {
